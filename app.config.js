2dea4ddb0548713c88acc2343e99848b38996d04
egazzolo1984 <egazzolo1984@gmail.com>
2026-04-28 00:16:45 -0500

    1.0.5: bump version

--- app.config.js
+++ app.config.js
@@ -3,7 +3,7 @@ module.exports = {
     name: "Veetha",
     slug: "Veetha",
     scheme: "veetha",
-    version: "1.0.4",
+    version: "1.0.5",
     orientation: "portrait",
     icon: "./assets/icon.png",
     userInterfaceStyle: "light",
