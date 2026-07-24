2b85b9a5e76c8d908514f5e311b52d5f584ef5e4
egazzolo <egazzolo1984@gmail.com>
2026-07-23 22:40:57 -0500

    Bump build number for App Store submission

--- app.config.js
+++ app.config.js
@@ -62,7 +62,7 @@ module.exports = {
     ios: {
       icon: "./assets/LogoAppStore.png",
       bundleIdentifier: "com.yourname.veetha",
-      buildNumber: "70",
+      buildNumber: "71",
       usesAppleSignIn: true,
       associatedDomains: ["applinks:nonotenb.com"],
       infoPlist: {
