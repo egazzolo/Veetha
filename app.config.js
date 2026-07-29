7ce401af1a58677de9264995a25c2675980149af
egazzolo <egazzolo1984@gmail.com>
2026-07-29 09:04:46 -0500

    Bump build number

--- app.config.js
+++ app.config.js
@@ -31,7 +31,7 @@ module.exports = {
     android: {
       package: "com.yourname.veetha",
       googleServicesFile: "./google-services.json",
-      versionCode: 42,
+      versionCode: 43,
       icon: "./assets/adaptive-icon.png",
       adaptiveIcon: {
         foregroundImage: "./assets/adaptive-icon.png",
