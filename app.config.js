f28ba3f1b5cc239258b06f59ce2dc32c64739e8e
egazzolo <egazzolo1984@gmail.com>
2026-08-14 13:23:15 -0500

    Next bump, 96

--- app.config.js
+++ app.config.js
@@ -66,7 +66,7 @@ module.exports = {
     ios: {
       icon: "./assets/LogoAppStore.png",
       bundleIdentifier: "com.yourname.veetha",
-      buildNumber: "95",
+      buildNumber: "96",
       usesAppleSignIn: true,
       associatedDomains: ["applinks:nonotenb.com"],
       infoPlist: {
